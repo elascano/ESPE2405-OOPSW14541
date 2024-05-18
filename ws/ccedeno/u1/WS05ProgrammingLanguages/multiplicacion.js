a4c615340538ccf051426bea56a7bd634fd2b192
AndresIsaiasCedenoCuenca <aicedeno1@espe.edu.ec>
2024-05-17 23:01:43 -0500

    ccedeno

---
+++ ws/ccedeno/u1/WS05ProgrammingLanguages/multiplicacion.js
@@ -0,0 +1,8 @@
+
+    for( i=1;i <=10;i++){
+        console.log(`tabla del ${i}:`);
+        for( j=1;j <= 5; j++){
+            console.log(`${i} x ${j}=${i*j}`);
+        }
+        console.log('');
+    }
