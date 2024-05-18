74fc2a9fbc10ad3112b13dccf1e802e08667f15c
saraycanarteg <sacanarte@espe.edu.ec>
2024-05-17 19:41:52 -0500

    canarte

---
+++ ws/canarte/u1/WS05ProgrammingLanguages/WS05MultiplicationTable.py
@@ -0,0 +1,9 @@
+def tabla_multiplicar_7():
+  """
+  Imprime la tabla de multiplicar del 7.
+  """
+  for i in range(1, 11):
+    resultado = 7 * i
+    print(f"7 x {i} = {resultado}")
+
+tabla_multiplicar_7()
