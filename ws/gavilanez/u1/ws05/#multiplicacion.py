bf66d8c769662d8d3ea3ad55a94ad391761f29ec
KennyJesusGavilanezOcampo <kennygavilanez2000@gmail.com>
2024-05-18 01:13:00 -0500

    submit ws

--- ws/gavilanez/u1/ws05/#multiplicacion.py
+++ ws/gavilanez/u1/ws05/#multiplicacion.py
@@ -1,7 +1,7 @@
+#multiplicacion
 factor = 5 #factor deseado, en este caso, haremos la tabla del 5
 desde = 1 #límite inferior (incluido)
 hasta = 15 #límite superior (incluido también)
 
 for f in range(desde, hasta + 1):
 	print(f'{factor} x {f} =  {factor * f}')
-    
