90da285fbef0009e4c95906d95f880bbc2ef7e8a
Carlos David Pantoja Jiménez <davidpantoja2311@hotmail.com>
2024-05-22 21:22:42 -0500

    pantoja

---
+++ ws/pantoja/u1/ws07JavaHelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    	public static void main(String args[]) {
+        	System.out.println("Hello future engineers from David Pantoja");
+        int addend1 = 4;
+        int addend2 = 5;
+        int sum;
+        sum = addend1 + addend2;
+        System.out.println("The result of adding" + addend1 + " + " + addend2 + " is --> " + sum);
+    }
+}
