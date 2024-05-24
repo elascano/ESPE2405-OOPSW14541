605c17b9fb51b1ca9e00931cf7da05a357691085
bpgualotuna <brayan.gualotuna09@gmail.com>
2024-05-23 20:55:41 -0500

    Chicken Farm Simulator

---
+++ ws/gualotuna/u1/ws_08CDforJava/ChickenFarmSimulator2/src/chickenfarmsimulator2/ChickenFarmSimulator2.java
@@ -0,0 +1,12 @@
+
+package chickenfarmsimulator2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ChickenFarmSimulator2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Chicken Farm Simulator");
+    }
+}
