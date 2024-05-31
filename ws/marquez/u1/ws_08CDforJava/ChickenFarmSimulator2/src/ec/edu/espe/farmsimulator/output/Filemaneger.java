4f48d577d78a685982cddd3b7cad7b4d8b9d8228
JenniMQ <marquezqjenniffer@gmail.com>
2024-05-30 23:32:31 -0500

    ws_11

---
+++ ws/marquez/u1/ws_08CDforJava/ChickenFarmSimulator2/src/ec/edu/espe/farmsimulator/output/Filemaneger.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.farmsimulator.output;
+
+import ec.edu.espe.farmsimulator.model.Chicken;
+
+/**
+ *
+ * @author Jenniffer Marquez, Javasquad, DCCO-ESPE
+ */
+class Filemaneger {
+
+    static void save(Chicken chicken) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
