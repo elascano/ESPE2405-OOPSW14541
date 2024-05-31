e5b5ee57322ba1b3a417045a8db76e751736f076
LuisEduardoVacaZurita <levaca4@espe.edu.ec>
2024-05-31 17:45:21 -0500

    WS11 Collection

---
+++ ws/Vaca/u1/WS11Collection/collections/src/ec/edu/espe/collection/model/Person.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.collection.model;
+
+/**
+ *
+ * @author luis Vaca,Code Creators,DCCO-ESPE
+ */
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "person{" + "id=" + getId() + ", name=" + getName() + '}';
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+            
+       
+    
+}
