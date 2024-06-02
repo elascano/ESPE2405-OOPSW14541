d314fad0335a9a79f49039c0acb6b477a33bfdae
adarboleda <adarboleda@espe.edu.ec>
2024-06-02 12:29:48 -0500

    arboleda

---
+++ ws/arboleda/u1/New Folder/PrimitiveData/src/PrimitveData/PrimitveData.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package PrimitveData;
+
+/**
+ *
+ * @author Abner Arboleda, Logic Legion, DCCO-ESPE
+ */
+public class PrimitveData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Short minuend; 
+    short subtrahend;
+    short difference;
+    
+    minuend=(short)70000;
+        System.out.println(minuend);
+        
+        float addend1=1.2F;
+        float addend2=2.4F;
+        float sum;
+        sum=addend1+addend2;
+        System.out.println(sum);
+        
+        char vowell='5';
+        subtrahend=(short)vowell;
+       difference=(short)(minuend-subtrahend);
+        System.out.println(subtrahend);
+        
+        System.out.println(difference);
+    }
+    
+}
