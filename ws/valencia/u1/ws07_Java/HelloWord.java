2c6b9c7e57cb56a8937160103ac576536915a4fe
Yulianav1 <valenciayuliana123@gmail.com>
2024-06-03 20:27:04 -0500

    valencia

---
+++ ws/valencia/u1/ws07_Java/HelloWord.java
@@ -0,0 +1,11 @@
+
+class HelloWorld{
+	public static void main(String args []) {
+		System.out.println("Hello future engineers from Yuliana Valencia");
+		int addend1 = 24;
+		int addend2 = 35;
+		int sum;
+		sum = addend1 + addend2;	
+		System.out.println("The result of adding " +addend1 + "+" +addend2 + "is:"+sum);
+	}
+}
---
+++ ws/valencia/u1/ws08FromClassDiagramToCode/ws_08ChickenFarmSimulator/src/ec/edu/espe/chikenfarmsimulator/model/Chicken.java
@@ -0,0 +1,164 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.chikenfarmsimulator.model;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Chicken {
+
+    private int id;
+    private String color;
+    private String name;
+    private String age;
+    private boolean molting;
+    private int bornOnYear;
+    private int bornOnMonth;
+    private int bornOnDay;
+    
+    public Chicken(int id, String name, String color, boolean molting, int year, int month, int day) {
+        this.id = id;
+        this.color = color;
+        this.name = name;
+        this.molting = molting;
+        this.bornOnYear = year;
+        this.bornOnMonth = month;
+        this.bornOnDay = day;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the molting
+     */
+    public boolean isMolting() {
+        return molting;
+    }
+
+    /**
+     * @param molting the molting to set
+     */
+    public void setMolting(boolean molting) {
+        this.molting = molting;
+    }
+
+    /**
+     * @return the bornOnYear
+     */
+    public int getBornOnYear() {
+        return bornOnYear;
+    }
+
+    /**
+     * @param bornOnYear the bornOnYear to set
+     */
+    public void setBornOnYear(int bornOnYear) {
+        this.bornOnYear = bornOnYear;
+    }
+
+    /**
+     * @return the bornOnMonth
+     */
+    public int getBornOnMonth() {
+        return bornOnMonth;
+    }
+
+    /**
+     * @param bornOnMonth the bornOnMonth to set
+     */
+    public void setBornOnMonth(int bornOnMonth) {
+        this.bornOnMonth = bornOnMonth;
+    }
+
+    /**
+     * @return the bornOnDay
+     */
+    public int getBornOnDay() {
+        return bornOnDay;
+    }
+
+    /**
+     * @param bornOnDay the bornOnDay to set
+     */
+    public void setBornOnDay(int bornOnDay) {
+        this.bornOnDay = bornOnDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Chicken{" + "id=" + id + ", color=" + color + ", name=" + name + ", age=" + age + ", molting=" + molting + ", bornOnYear=" + bornOnYear + ", bornOnMonth=" + bornOnMonth + ", bornOnDay=" + bornOnDay + '}';
+    }
+  
+    public String toString(int type) {
+        String chickenData = "";
+        
+        if(type == 1){
+            chickenData = id + ", " + name + ", " + color + ", " + molting + ", " + bornOnYear + ", " + bornOnMonth + ", " + bornOnDay;
+        }
+        return chickenData;
+    }
+
+
+}
+  
+  
+    
+
+    
