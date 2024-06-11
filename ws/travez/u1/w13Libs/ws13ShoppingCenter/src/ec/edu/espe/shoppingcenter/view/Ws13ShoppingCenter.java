6269ddb8c82a5cce7e31f3523de452f6e2fef1a8
Travez Cachago Alex Ismael <ismael1_2004@hotmail.com>
2024-06-11 15:56:01 -0500

    ws13

---
+++ ws/travez/u1/w13Libs/ws13ShoppingCenter/src/ec/edu/espe/shoppingcenter/view/Ws13ShoppingCenter.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ec.edu.espe.shoppingcenter.view;
+
+import ec.edu.espe.shoppingcenter.model.CellPhone;
+
+/**
+ *
+ * @author Ismael Travez, The Java Bandits, DCCO-ESPE
+ */
+public class Ws13ShoppingCenter {
+    public static void main(String[] args) {
+        float totalPrice;
+        CellPhone cellPhone = new CellPhone(001, "Samsung", "white", 150.00F);
+        
+        System.out.println(cellPhone);
+        
+    }
+    
+}
