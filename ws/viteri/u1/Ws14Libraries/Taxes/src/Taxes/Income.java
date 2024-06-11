28c553c8c2adf992ad44ad87b26f74e8028ca13a
Viteri <alexisviterigithub@gmail.com>
2024-06-11 17:31:27 -0500

    viteri

---
+++ ws/viteri/u1/Ws14Libraries/Taxes/src/Taxes/Income.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taxes;
+
+/**
+ *
+ * @author Alexis Viteri DCO-ESPE
+ */
+public class Income {
+    public float computeIR(float salary){
+    return 0.1F;
+    }
+}
