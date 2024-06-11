e4c40263a8a79cc8a8844f9acf005b82cb575971
Viteri <alexisviterigithub@gmail.com>
2024-06-11 17:31:27 -0500

    viteri

---
+++ ws/viteri/u1/Ws14Libraries/Shoping Center/src/shoping/center/ShopingCenter.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shoping.center;
+
+import model.CellPhone;
+
+/**
+ *
+ * @author Alexis Viteri DCO-ESPE
+ */
+public class ShopingCenter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float totalPrice;
+        CellPhone cellPhone; 
+        
+        cellPhone = new CellPhone(0, "Chinito", "yellow", 2000.F);
+        
+        System.out.println("cell ---> " + cellPhone);
+        
+    }
+    
+}
