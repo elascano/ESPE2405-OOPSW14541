2bcead8d3f3ae35b17e0cead6d7a29540572a35f
Nahomi Cedeno <naho26cede@gmail.com>
2024-06-11 13:44:14 -0500

    ws14 library

---
+++ ws/CEDENOR/U1/Workshops/WS14Taxes/WS13Taxes/src/taxes/Income.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taxes;
+
+/**
+ *
+ * @author Nahomi Cedeno
+ */
+public class Income {
+    public float computeIR(float salary){
+        //1000
+        return 0.1F;
+    }
+}
