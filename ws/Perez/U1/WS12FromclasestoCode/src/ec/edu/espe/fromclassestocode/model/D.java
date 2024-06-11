88e891f0d9d4805dfe1777d596c98e1e49ce5379
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-06-11 17:59:20 -0500

    Pérez

---
+++ ws/Perez/U1/WS12FromclasestoCode/src/ec/edu/espe/fromclassestocode/model/D.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.fromclassestocode.model;
+
+/**
+ *
+ * @author Brayan Gualotuña, Dev Dynasty, DCCO-ESPE
+ */
+public class D {
+
+    @Override
+    public String toString() {
+        return "D{" + '}';
+    }
+    
+}
