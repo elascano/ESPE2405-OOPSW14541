6e4cc28edf3cd960c4cda382739ecac0fce696d1
domenicole <memimika0809@gmail.com>
2024-06-11 23:21:21 -0500

    Homework 11 - ws 11 and 12

---
+++ ws/villagomez/u1/ws08FromClassDiagramToCode/ChickenFarmSimulator2/src/utils/ControlData.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Doménica Villagómez, CommitCrew, DCCO-ESPE
+ */
+public class ControlData {
+    
+    public static void csvFile(ArrayList<Class<String>){
+        
+        
+        /*BufferedReader getBuffered(String chickenData){
+        FileReader lector = null;
+        BufferedReader br = null;
+*/
+        
+        
+        
+    }
+    public void readFile(){
+        
+    }
+}
