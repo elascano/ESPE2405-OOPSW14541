f5265a7183167772a79bde6372ee10c64e9038b5
domenicole <memimika0809@gmail.com>
2024-06-11 23:21:21 -0500

    Homework 11 - ws 11 and 12

---
+++ hw/villagomez/u1/hw11ObjectInFile/MusicStore/src/ec/edu/espe/musicstudio/model/MusicalInstrument.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.musicstudio.model;
+
+/**
+ *
+ * @author Doménica Villagómez, CommitCrew, DCCO-ESPE
+ */
+public class MusicalInstrument {
+    private int id;
+    private String name;
+    private String type;
+    private String difficulty;
+
+    public MusicalInstrument(int id, String name, String type, String difficulty) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicalInstrument{" + "id=" + id + ", name=" + name + ", type=" + type + ", difficulty=" + difficulty + '}';
+    }
+
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the difficulty
+     */
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * @param difficulty the difficulty to set
+     */
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    
+}
---
+++ ws/villagomez/u1/ws12_FromClassesToCode/src/ec/edu/espe/fromclassestocode/model/D.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.fromclassestocode.model;
+
+/**
+ *
+ * @author Domenica Villagomez, CommitCrew, DCCO-ESPE
+ */
+public class D {
+
+    @Override
+    public String toString() {
+        return "D{" + '}';
+    }
+    
+}
