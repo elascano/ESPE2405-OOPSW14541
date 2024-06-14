99f67bd0b8ef5b6f7cd7296bca2067a9abcc0c6b
LisintunaCorreaCristianMateo <cmlisintuna@espe.edu.ec>
2024-06-14 15:28:45 -0500

    Ws

---
+++ ws/lisintuña/u1/WS15/JavaApplication6/src/espe/edu/ec/referentsDT/view/ReferencedDT.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espe.edu.ec.referentsDT.view;
+
+/**
+ *
+ * @author DELL
+ */
+public class ReferencedDT {
+    public static void main(String() args){
+        float salaty;
+        Professor professor =new Professor(id:1,name:"Edison",salary:200,53);
+        float salaries[]=new float[3];
+        salaries[0]=1;
+        salaries[0]=2;
+        salaries[0]=3;
+        System()
+    }
+    public static void changesalaries(float salaries[]){
+        System.out.println("Salary from main after calling ");
+        salaries[0]=1000;
+        salaries[0]=1000;salaries[0]=1000;
+        
+    }
+}
