de76b978a11060467f4378d941c83c19725feb1e
Nahomi Cedeno <naho26cede@gmail.com>
2024-06-13 22:10:05 -0500

    add new

---
+++ hw/CEDENOR/U1/HOMEWORK/HW08HelloWord/HelloWorld.js
@@ -0,0 +1,11 @@
+class HelloWorld{
+    static main() {
+        console.log("Hello future engineers from Nahomi Cedeño");
+        let addend1 = 4;
+        let addend2 = 5;
+        let sum = addend1 + addend2;
+        console.long("The result of adding" +addend1 + " + " + addend2 + addend2 + " is " + sum);
+    }
+}
+
+HelloWorld.main();
