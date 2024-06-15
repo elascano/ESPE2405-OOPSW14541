f67d02a33a2a0d02790ae870a18857afb0bdbcd2
Carlos David Pantoja Jiménez <davidpantoja2311@hotmail.com>
2024-06-14 23:56:28 -0500

    update

---
+++ ws/pantoja/u1/ws14Libs/Taxes/src/taxes/income.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taxes;
+
+/**
+ *
+ * @author David Pantoja, JavaSquad, DCCO-ESPE
+ */
+public class income {
+    public static float computeIR(float salary){
+        // todo
+        return 0.1F;
+    }
+}
