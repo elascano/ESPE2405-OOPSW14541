1cb1287e5d5bac2f6d93fd4bab107467151e5a00
KennyJesusGavilanezOcampo <kennygavilanez2000@gmail.com>
2024-06-15 00:34:23 -0500

    Update v 0.1

--- hw/gavilanez/u1/hw11ObjectFile/ObjectBicycle/src/ec/edu/espe/shopping/view/ObjectBicycle.java
+++ hw/gavilanez/u1/hw11ObjectFile/ObjectBicycle/src/ec/edu/espe/shopping/view/ObjectBicycle.java
@@ -3,21 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ec.edu.espe.shopping.view;
-import ec.edu.espe.shopping.model.*;
 
-
+import ec.edu.espe.shopping.model.Bicycle;
 /**
  *
  * @author Kenny Gavilanez, Dev Dynasty, DCCO-ESPE
  */
-public class ShoppingCenter {
+public class ObjectBicycle {
     public static void main(String[] args) {
         float totalPrice;
+        Bicycle bicycle;
+        bicycle= new Bicycle("Scott","Montañera","black",2,1500.0F,15.0F);
         
-        CellPhone cellPhone;
-        cellPhone = new CellPhone(0,"motorola","black",2000.0F,15.0F);
-        System.out.println("cellphone ---> " + cellPhone);
-        FileManager.save(cellPhone.toString(),"cellPhone",1);
-
-    } 
+        System.out.println("bicycle ---> " + bicycle);
+        
+        float salary =2000;
+        
+    }
 }
