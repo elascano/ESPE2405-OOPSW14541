a547167f13da832f46673315fb3e0ae97f347ad0
cmbonifaz <mateobvc@hotmail.com>
2024-06-15 00:36:15 -0500

    Upload files bonifaz

---
+++ ws/bonifaz/u1/ws12FromClassesToCode/FromClassesToCode/src/ec/edu/espe/fromclassestocode/model/D.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.fromclassestocode.model;
+
+/**
+ *
+ * @author Christian Bonifaz, Code Creators, DCCO-ESPE
+ */
+public class D {
+
+    @Override
+    public String toString() {
+        return "D{" + '}';
+    }
+    
+}
