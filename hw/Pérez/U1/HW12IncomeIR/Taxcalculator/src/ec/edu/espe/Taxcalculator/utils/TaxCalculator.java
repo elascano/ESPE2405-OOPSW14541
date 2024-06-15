ae866979bb74b0322aa01107729af9963542c069
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-06-14 19:22:20 -0500

    HW12

--- hw/Pérez/U1/HW12IncomeIR/Taxcalculator/src/ec/edu/espe/Taxcalculator/utils/TaxCalculator.java
+++ hw/Pérez/U1/HW12IncomeIR/Taxcalculator/src/ec/edu/espe/Taxcalculator/utils/TaxCalculator.java
@@ -8,13 +8,20 @@ import java.util.List;
  * @author Carlos Perez,The Javas, DCCO-ESPE
  */
 public class TaxCalculator {
-    public static double Tax (double annualincome,List<Section> sections ){
+    
+    public static double Taxiess(double mensualincome){
+        
+        
+        return (mensualincome*12) * 0.0945;
+    }
+    
+    public static double Tax (double mensualincome,List<Section> sections ){
         for (Section section : sections){
-            if (annualincome >= section.getLowerlimit() && annualincome <= section.getUpperlimit()){
+            if (mensualincome >= section.getLowerlimit() && mensualincome <= section.getUpperlimit()){
                 double basetax = section.getBasetax();
                 double excesspercentage= section.getExcesspercentage();
                 double lowerlimit = section.getLowerlimit();
-                double annualtax = basetax + (annualincome - lowerlimit) * excesspercentage;
+                double annualtax = basetax + (mensualincome - lowerlimit) * excesspercentage;
                 return Math.round(annualtax * 100.0) / 100.0;
                                 
             }
