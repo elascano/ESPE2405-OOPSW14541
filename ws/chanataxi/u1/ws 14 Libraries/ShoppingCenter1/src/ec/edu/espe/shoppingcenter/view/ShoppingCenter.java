8916b90f3ddf5f9d2542c33704b50153052b930a
marcoviniciochanataxiquimbiamba <chanataximarco12@gmail.com>
2024-06-17 20:28:00 -0500

    chanataxi

---
+++ ws/chanataxi/u1/ws 14 Libraries/ShoppingCenter1/src/ec/edu/espe/shoppingcenter/view/ShoppingCenter.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ec.edu.espe.shoppingcenter.view;
+
+import ec.edu.espe.shoppingcenter.model.CellPhone;
+
+/**
+ *
+ * @author Marco Chanataxi, Dev Dynasty, DCCO-ESPE
+ */
+public class ShoppingCenter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        float totalPrice;
+        CellPhone cellPhone;
+        
+        cellPhone = new CellPhone(0,"motorola","black",2000.0F,15.0F);
+        
+        System.out.println("cellphone ---> " + cellPhone);
+    }
+}
+    
+    
+
