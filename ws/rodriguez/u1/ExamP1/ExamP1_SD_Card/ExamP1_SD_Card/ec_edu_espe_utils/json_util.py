3b586c3117655c94034d81cf21968f683dbfa56f
JJOJJD <david2626714@gmail.com>
2024-06-18 19:34:50 -0500

    Exam

--- ws/rodriguez/u1/ExamP1/ExamP1_SD_Card/ExamP1_SD_Card/ec_edu_espe_utils/json_util.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def write_json(file_path, data):
-    with open(file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
-
-def read_json(file_path):
-    with open(file_path, 'r') as json_file:
-        return json.load(json_file)
