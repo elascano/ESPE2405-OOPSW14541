7d63175d1d070c7ab4a50fb66a551afe4370f7c8
Viteri <alexisviterigithub@gmail.com>
2024-06-23 11:59:54 -0500

    viteri

---
+++ ws/viteri/u1/WS12FromClasesToCode/src/view/WS12FromClasesToCode.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import model.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alexis Viteri DCO-ESPE
+ */
+public class WS12FromClasesToCode {
+    public static void main(String[] args) {
+        A a;
+        B b;
+        C c;
+        D d;
+        E e;
+        ArrayList<C> cs = new ArrayList<>();
+        C otherCs[] = new C[10];
+                
+        c = new C();
+        d = new D();
+        
+        cs.add(c);
+        cs.add(new C());
+        cs.add(c);
+        
+        otherCs [1] = c;
+        
+        b = new B(0.1F, otherCs);
+        e = new E(cs);
+        a = new A(1, b);
+        
+        System.out.println("d----> " + d);
+        System.out.println("c ---> " + c);
+        System.out.println("e ---> " + e);
+        
+        System.out.println("b --> " + b);
+        System.out.println("a ---> " + a);
+        
+        System.out.println("running a method --> " + a.m(d));
+        
+        cs.forEach((otherC) -> System.out.println(otherC));
+        
+        
+        
+    }
+}
