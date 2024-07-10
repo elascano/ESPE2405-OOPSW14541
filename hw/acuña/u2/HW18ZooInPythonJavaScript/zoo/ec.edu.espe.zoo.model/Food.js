d0720280b4179817ae76f9c2ba761eaab11c5b84
Christian Marcelo Acuña Gamboa <crissre456@hotmail.com>
2024-07-10 11:02:12 -0500

    HW18 Acuña

---
+++ hw/acuña/u2/HW18ZooInPythonJavaScript/zoo/ec.edu.espe.zoo.model/Food.js
@@ -0,0 +1,24 @@
+class Food {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+}
+
+module.exports = { Food };
