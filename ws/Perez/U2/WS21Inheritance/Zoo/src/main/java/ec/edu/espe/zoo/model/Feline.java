8683a748a0f0d1a08f9ed6078c470e8a1d923634
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-07-13 00:13:25 -0500

    WS

---
+++ ws/Perez/U2/WS21Inheritance/Zoo/src/main/java/ec/edu/espe/zoo/model/Feline.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.zoo.model;
+import java.util.Date;
+/**
+ *
+ * @author Carlos Perez,The Javas, DCCO-ESPE
+ */
+public abstract class Feline extends Mammal{
+    
+    public Feline(int id, String description, Date bornOnDate, char gender, Cage cage, int numberOfBones, boolean vetebrate) {
+        super(id, description, bornOnDate, gender, cage, numberOfBones, vetebrate);
+    }
+    
+}
