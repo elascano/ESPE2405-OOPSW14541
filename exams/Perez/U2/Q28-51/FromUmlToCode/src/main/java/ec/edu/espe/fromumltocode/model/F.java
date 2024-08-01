b051dadb3fd22ce7aef13c5473dfd9f6f8f3b857
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-08-01 18:32:11 -0500

    Exam

---
+++ exams/Perez/U2/Q28-51/FromUmlToCode/src/main/java/ec/edu/espe/fromumltocode/model/F.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.fromumltocode.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author G406
+ */
+public class F {
+    private List<F> fList = new ArrayList<>();
+
+    public void addF(F f) {
+        fList.add(f);
+    }
+    
+}
