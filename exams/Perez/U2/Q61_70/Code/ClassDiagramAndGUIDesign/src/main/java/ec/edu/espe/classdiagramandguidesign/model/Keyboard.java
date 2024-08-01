a58bff3cde895d94511dea5f31ecf3f652d615d0
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-08-01 18:32:11 -0500

    Exam

---
+++ exams/Perez/U2/Q61_70/Code/ClassDiagramAndGUIDesign/src/main/java/ec/edu/espe/classdiagramandguidesign/model/Keyboard.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.classdiagramandguidesign.model;
+
+/**
+ *
+ * @author G406
+ */
+public class Keyboard {
+    String id;
+    String name;
+    double numericAttribute1;
+    double numericAttribute2;
+    
+}
