c5de9532709c98a2fd1ffdcddad175240eba238d
LisintunaCorreaCristianMateo <cmlisintuna@espe.edu.ec>
2024-08-01 21:05:54 -0500

    update

---
+++ ws/lisintuña/u2/WS30/AbstractClassesQuiz/src/main/java/utils/CvsManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author DELL
+ */
+public class CvsManager extends FileManager { //1.5
+    
+    
+}
