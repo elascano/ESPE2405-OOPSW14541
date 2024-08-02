db768d682fc4aff2a6d445e593adc754c42de557
NAHOMICEDENO <naho26cede@gmail.com>
2024-08-01 20:10:55 -0500

    cedenor

---
+++ hw/CEDENOR/U2/HW17TestCaseSolution/ComplexOperations/src/main/java/ec/edu/espe/complexoperations/view/OperationPrinting.java
@@ -0,0 +1,17 @@
+
+package ec.edu.espe.complexoperations.view;
+
+
+/**
+ *
+ * @author Nahomi Cedeño, codecreators, DCCO-ESPE
+
+ *
+ */
+public class OperationPrinting {
+
+    public static void printAddition(float addend1, float addend2, float sum) {
+        System.out.println("The addition of " + addend1 + "+" + addend2 + "=" + sum);
+    }
+
+}
