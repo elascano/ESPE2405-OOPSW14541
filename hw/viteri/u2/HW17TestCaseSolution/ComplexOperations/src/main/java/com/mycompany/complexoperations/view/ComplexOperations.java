c55844a5b452269c7591cf9b32a802b2b767e588
Viteri <alexisviterigithub@gmail.com>
2024-08-02 17:54:35 -0500

    viteri

---
+++ hw/viteri/u2/HW17TestCaseSolution/ComplexOperations/src/main/java/com/mycompany/complexoperations/view/ComplexOperations.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.complexoperations.view;
+
+import controller.Operations;
+import controller.PersonController;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Alexis Viteri DCO-ESPE
+ */
+public class ComplexOperations {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Complex Operation by Alexis Viteri");
+        double addend1;
+        double addent2;
+        double sum;
+        
+        
+        addend1=1;
+        addent2=2;
+        sum=Operations.addTwoNumbers(addend1, addent2);
+        OperationPrinting.printAddition(addend1, addent2, sum);
+        
+        addend1=2.2F;
+        addent2=2.6F;
+        sum=Operations.addTwoNumbers(addend1, addent2);
+        OperationPrinting.printAddition(addend1, addent2, sum);
+        
+        addend1=2.2F;
+        addent2=-1.5F;
+        sum=Operations.addTwoNumbers(addend1, addent2);
+        OperationPrinting.printAddition(addend1, addent2, sum);
+        
+        LocalDate birthDate = LocalDate.parse("2004-02-04");
+       int years;
+        int days = PersonController.computeAgeInDays(birthDate);
+        years = PersonController.computeAgeInYears(birthDate);
+        System.out.println("Alexis is "+days+" days old");
+        System.out.println("He was born on "+birthDate);
+        
+        System.out.println("Alexis is "+ years+ " years old");
+    }
+
+
+
+}
