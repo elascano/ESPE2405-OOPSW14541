78010fa88919a494729a1a8e33384642a471a7e8
Eduardo Andres Segarra Diaz <edusegarra6@gmail.com>
2024-08-05 11:16:26 -0500

    segarra

---
+++ exams/segarra/u2/Q6170/src/main/java/ec/edu/espe/q6170/controller/CloudController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.q6170.controller;
+    
+import com.google.gson.Gson;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.ConnectionString;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import javax.swing.text.Document;
+
+/**
+ *
+ * @author Eduardo Segarra
+ */
+public class CloudController {
+    public static boolean create(Object object) {
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://segarra:segarra@cluster0.b2q6ac3.mongodb.net/");
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase database = mongoClient.getDatabase("Q6170");
+
+            MongoCollection<Document> collection = database.getCollection("Stores");
+
+            Gson gson = new Gson();
+            String json = gson.toJson(object);
+            Document studentDocument = Document.parse(json);
+
+            collection.insertOne(studentDocument);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
