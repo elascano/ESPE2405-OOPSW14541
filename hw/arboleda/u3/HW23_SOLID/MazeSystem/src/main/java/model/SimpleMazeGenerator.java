270fa5a5fec301f9461b4786eab2e0c174014a3f
adarboleda <adarboleda@espe.edu.ec>
2024-08-14 08:25:58 -0500

    arboleda

---
+++ hw/arboleda/u3/HW23_SOLID/MazeSystem/src/main/java/model/SimpleMazeGenerator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Abner Arboleda, Logic Legion, DCCO-ESPE
+ */
+public class SimpleMazeGenerator implements MazeGenerator {
+    @Override
+    public void generate(Room[][] rooms) {
+        System.out.println("Generanting Rooms...");
+    }
+}
