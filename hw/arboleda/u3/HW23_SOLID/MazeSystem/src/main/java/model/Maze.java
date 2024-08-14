d960fc66eb09fae63175f2d2d5f4d7666825b03f
adarboleda <adarboleda@espe.edu.ec>
2024-08-14 08:25:58 -0500

    arboleda

---
+++ hw/arboleda/u3/HW23_SOLID/MazeSystem/src/main/java/model/Maze.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Abner Arboleda, Logic Legion, DCCO-ESPE
+ */
+public class Maze {
+    private int width, height;
+    private Room[][] rooms;
+
+    public Maze(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.rooms = new Room[width][height];
+    }
+
+    public Room[][] getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Room[][] rooms) {
+        this.rooms = rooms;
+    }
+
+    /**
+     * @return the width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+}
