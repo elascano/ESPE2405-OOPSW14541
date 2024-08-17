4df85f2c2d01076272cab4cedc95195999460ebf
Kenny Jesus Gavilanez Ocampo <kennygavilanez2000@gmail.com>
2024-08-17 16:05:49 -0500

    Update all

---
+++ hw/gavilanez/u3/HW23Solid/MazeAplication/src/main/java/ec/edu/espe/mazeaplication/controller/DoorController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.mazeaplication.controller;
+
+import ec.edu.espe.mazeaplication.model.Door;
+
+/**
+ *
+ * @author Kenny Gavilanez, Dev Dynasty, DCCO-ESPE
+ */
+public class DoorController {
+    public void addDoor(Door door) {
+        // Lógica para agregar una puerta
+    }
+}
