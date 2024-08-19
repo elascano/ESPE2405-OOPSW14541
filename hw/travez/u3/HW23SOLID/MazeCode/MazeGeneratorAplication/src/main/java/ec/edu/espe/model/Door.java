08c355c3eaa326eaf62cc950e8ef0dba924c3bb2
Travez Cachago Alex Ismael <ismael1_2004@hotmail.com>
2024-08-18 22:27:49 -0500

    hw23

---
+++ hw/travez/u3/HW23SOLID/MazeCode/MazeGeneratorAplication/src/main/java/ec/edu/espe/model/Door.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.model;
+
+import ec.edu.espe.controller.MazeComponent;
+
+/**
+ *
+ * @author Ismael Travez, The Java Bandits, DCCO-ESPE
+ */
+public class Door extends RoomElement{
+    public void generate() {
+        System.out.println("Door generated.");
+    }
+}
---
+++ hw/travez/u3/HW23SOLID/MazeCode/MazeGeneratorAplication/src/main/java/ec/edu/espe/model/Maze.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.model;
+
+import ec.edu.espe.controller.MazeComponent;
+import java.util.List;
+
+/**
+ *
+ * @author Ismael Travez, The Java Bandits, DCCO-ESPE
+ */
+public class Maze {
+    private final List<MazeComponent> components = null;
+}
