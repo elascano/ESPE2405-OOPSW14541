6b535e88fe5e63390da25358f09b6cd5ef436e98
(ayuquina) <dmayuquina@espe.edu.ec>
2024-08-20 23:33:04 -0500

    ayuquina

---
+++ hw/ayuquina/u3/hw28DesignPatterns/AbstractFactoryPattern/AbstractFactory/src/main/java/ec/edu/espe/abstractfactory/controller/LinuxMenu.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.abstractfactory.controller;
+
+/**
+ *
+ * @author Danny Ayuquina, LogicLegion, DCCO-ESPE
+ */
+public class LinuxMenu extends Menu {
+
+    @Override
+    public void paint() {
+        System.out.println("I'm a LinuxMenu " + caption);
+    }
+    
+}
