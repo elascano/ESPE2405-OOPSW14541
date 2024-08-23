a53603456cb30e6a9b5f384c7b92c1974abb86a1
JenniMQ <marquezqjenniffer@gmail.com>
2024-08-23 10:49:59 -0500

    marquez

---
+++ hw/marquez/u3/hw28DesignPatterns/hw28DesignPatterns/AbstractFactoryPattern/AbstractFactory/src/main/java/ec/edu/espe/abstractfactory/controller/Button.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.abstractfactory.controller;
+
+/**
+ *
+ * @author Jenniffer Marquez, Javasquad, DCCO-ESPE
+ */
+public abstract class Button {
+    public String caption;
+    public abstract void paint();
+}
