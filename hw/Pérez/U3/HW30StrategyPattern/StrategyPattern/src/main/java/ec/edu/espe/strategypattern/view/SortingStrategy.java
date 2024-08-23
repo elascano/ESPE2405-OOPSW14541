0a1288ef635a08629406286b5c1d9bbeaf3b462b
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-08-23 14:05:31 -0500

    HW30

---
+++ hw/Pérez/U3/HW30StrategyPattern/StrategyPattern/src/main/java/ec/edu/espe/strategypattern/view/SortingStrategy.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.strategypattern.view;
+
+/**
+ *
+ * @author Carlos Perez,The Javas, DCCO-ESPE
+ */
+public interface SortingStrategy {
+    public int[] sort(int data[]);
+    
+}
