3c8f24168b24bb68ba8809ba2eb145cd407cdef1
EnzoAlbertoArmijosMacas <armijosenzo@gmail.com>
2024-08-23 14:27:39 -0500

    hw

---
+++ hw/armijos/u3/hw29TemplateMethodStrategyComposite/Composite/Composite/src/main/java/ec/edu/espe/composite/model/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.composite.model;
+
+/**
+ *
+ * @author Enzo Armijos, LogicLegion, DCCO-ESPE
+ */
+public abstract class Employee {
+    String name = "not assigned yet";
+    String title = "not assigned yet";
+    
+    public void stateName(){
+        System.out.println(title + " " + name);
+    }
+}
---
+++ hw/armijos/u3/hw29TemplateMethodStrategyComposite/Strategy/StrategyPattern/src/main/java/ec/edu/espe/sortingapp/controller/SortingStrategy.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.sortingapp.controller;
+
+/**
+ *
+ * @author Enzo Armijos, LogicLegion, DCCO-ESPE
+ */
+public interface SortingStrategy {
+    int[] sort(int[] data);
+}
