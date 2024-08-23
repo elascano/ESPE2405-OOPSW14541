df3487ad74af98189948f7cde422878018410e05
adarboleda <adarboleda@espe.edu.ec>
2024-08-23 12:40:56 -0500

    arboleda

---
+++ hw/arboleda/u3/HW29TemplateAndCompsite/HW29CompositePattern/src/main/java/ec/edu/espe/event/hw29compositepattern/President.java
@@ -0,0 +1,28 @@
+package ec.edu.espe.event.hw29compositepattern;
+
+/**
+ *
+ * @author Abner Arboleda, LogicLegion, DCCO-ESPE
+ */
+public class President extends Supervisor{
+    private static President president = new President();
+    
+    private President(String aName){
+        this();
+        name = aName;
+    }
+    
+    private President(){
+        super();
+        title = "President";
+    }
+    
+    public void stateName(){
+        super.stateName();
+    }
+    
+    public static President getPresident(String aName){
+        president.name = aName;
+        return President.president;
+    }
+}
