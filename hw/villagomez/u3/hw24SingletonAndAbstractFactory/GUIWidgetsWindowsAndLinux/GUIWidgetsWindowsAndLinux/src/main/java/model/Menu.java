341ab77dd83df20310e97de207f145572c81dea0
domenicole <memimika0809@gmail.com>
2024-08-23 11:56:15 -0500

    Strategy hw and more

---
+++ hw/villagomez/u3/hw24SingletonAndAbstractFactory/GUIWidgetsWindowsAndLinux/GUIWidgetsWindowsAndLinux/src/main/java/model/Menu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Domenica Villagomez, CommitCrew, DCCO-ESPE
+ */
+public abstract class Menu {
+    protected String caption;
+
+    public Menu(String caption) {
+        this.caption = caption;
+        System.out.println("Creating Menu: " + caption);
+    }
+
+    public abstract void paint();
+}
