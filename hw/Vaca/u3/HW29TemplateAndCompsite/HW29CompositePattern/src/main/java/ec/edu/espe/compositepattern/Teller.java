d5f41bd4b751958fdc7117ef4336eed587be0043
LuisEduardoVacaZurita <levaca4@espe.edu.ec>
2024-08-24 18:36:28 -0500

    Hw29Composite

---
+++ hw/Vaca/u3/HW29TemplateAndCompsite/HW29CompositePattern/src/main/java/ec/edu/espe/compositepattern/Teller.java
@@ -0,0 +1,22 @@
+
+package ec.edu.espe.compositepattern;
+
+/**
+ *
+ * @author MSI-PULSE
+ */
+public class Teller extends Employee {
+
+    public Teller(String aName) {
+        this();
+        name = aName;
+    }
+
+    public void stateName() {
+        super.stateName();
+    }
+
+    public Teller() {
+        title = "Teller";
+    }
+}
