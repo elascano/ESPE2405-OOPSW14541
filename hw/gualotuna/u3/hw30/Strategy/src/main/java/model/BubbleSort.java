2ba13de155e621fa148b6288ed5ed18b29f4efdf
bpgualotuna <brayan.gualotuna09@gmail.com>
2024-08-28 13:45:33 -0500

    hw

---
+++ hw/gualotuna/u3/hw30/Strategy/src/main/java/model/BubbleSort.java
@@ -0,0 +1,33 @@
+package model;
+
+import controller.SortingStrategy;
+
+/**
+ *
+ * @author  Brayan Gualotuña  
+ */
+public class BubbleSort implements SortingStrategy {
+
+    @Override
+    public int[] sort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no elements were swapped, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+        return array;
+    }
+}
