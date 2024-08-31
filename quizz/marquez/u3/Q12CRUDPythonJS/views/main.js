8b94a744a11432f8c5791196a8372cf5f140644c
JenniMQ <marquezqjenniffer@gmail.com>
2024-08-31 13:45:40 -0500

    marquez

--- quizz/marquez/u3/Q12CRUDPythonJS/views/main.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-document.getElementById('createStudentBtn').addEventListener('click', () => {
-    ipcRenderer.send('navigate', 'CreateStudent.html');
-});
-
-document.getElementById('searchStudentBtn').addEventListener('click', () => {
-    ipcRenderer.send('navigate', 'SearchStudent.html');
-});
