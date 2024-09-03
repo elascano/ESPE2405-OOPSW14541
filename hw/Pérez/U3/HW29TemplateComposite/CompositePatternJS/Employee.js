814db080ad62b81372a22696bc184004cc7f26c0
PEREZ CONDOR CARLOS ANDRES <carlospe0505@gmail.com>
2024-09-03 16:47:05 -0500

    HW

---
+++ hw/Pérez/U3/HW29TemplateComposite/CompositePatternJS/Employee.js
@@ -0,0 +1,10 @@
+export class Employee {
+    constructor() {
+        this.name = "Not assigned yet";
+        this.title = "Not assigned yet";
+    }
+
+    stateName() {
+        console.log(`${this.title} ${this.name}`);
+    }
+}
