6057baa51293d874dcce65cf31240c99e707673d
JJOJJD <david2626714@gmail.com>
2024-09-03 18:07:23 -0500

    .

---
+++ hw/Rodriguez/u3/hw30/ObserverPatttern/src/main/java/ec/edu/espe/observerpattern/controller/IInvestor.java
@@ -0,0 +1,11 @@
+
+package ec.edu.espe.observerpattern.controller;
+
+/**
+ *
+ * @author David Rodriguez,THEJAVABANDITS DCCO-ESPE
+ */
+public interface IInvestor {
+    void update(Stock stock, Object args);
+}
+
